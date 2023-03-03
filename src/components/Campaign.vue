f217d5955485d30f995bce9033351ae1a77dfd15
Peter Dahlin <peterdaliandalledahlin@gmail.com>
2023-03-03 22:34:20 +0100

    mars campaign updated

--- src/components/Campaign.vue
+++ src/components/Campaign.vue
@@ -7,7 +7,10 @@
         <h2>Vi är med dig hela vägen från design till montering</h2>
         <h3><span>Kökskampanj</span> under mars månad, då vi har öppet;</h3>
         <p>Lördag 10:00-13:00 och Måndag 12:00-18:00, övrig tid efter tidsbokning</p>
-        <p>Torggatan 28C, info@kok-bygg.se, 0171-340 00</p>
+        <p>Torggatan 28C &bull; info@kok-bygg.se &bull; 0171-340 00</p>
+        <p>Köp kök under kampanj-perioden och <span>få 2.000:- per 10.000:-</span> som köket kostar att nyttja vid köp av vitvaror från vårt sortiment från <span>Miele</span> och <span>Witt</span></p>
+        <!-- <p>Köp kök under kampanj-perioden och <span>få 2.000:- per 10.000:-</span> att nyttja vid köp av vitvaror från vårt sortiment från <span>Miele</span> och <span>Witt</span></p> -->
+        <p>Gratis köksplanering under kampanj-perioden (ordinarie pris = 5.000:-)</p>
     </v-container>
 </template>
 
@@ -25,6 +28,7 @@
     text-align: center;
     background-color: #231F20;
 }
+.v-container > p:nth-child()
 h1 {
     font-size: clamp(1rem, -0.875rem + 8.333vw, 2rem);
 }
@@ -38,6 +42,29 @@ h2, h3, p {
     color: #fff;
     /* #FEF200 */
 }
+h3 > span {
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    font-style: italic;
+    font-size: x-large;
+    font-weight: 100;
+}
+p > span:nth-child(1) {
+    color: #FEF200;
+}
+p > span:nth-child(2) {
+    color: #fff;
+    background: red;
+    padding: 3px 3px;
+}
+p > span:nth-child(3) {
+    color: #000;
+    background: #fff;
+    padding: 3px 3px;
+}
+/* Köp kök under kampanj-perioden och få 2.000:- per 10.000:- som köket kostar
+               att nyttjas vid köp av vitvaror från vårt sortiment från MIELE och Witt.
+
+Gratis köksplanering under kampanj-perioden (ordinarie pris = 5.000:-) */
 .wrapper {
     display: flex;
     justify-content: center;
